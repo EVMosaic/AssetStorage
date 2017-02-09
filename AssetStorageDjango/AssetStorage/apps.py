34ac5539852a66c0f4064dd76089e960d573b89f
EVMosaic <evarela@mosaiclearning.com>
2017-02-09 15:56:11 -0500

    Setup virtualenv and Django project

---
+++ AssetStorageDjango/AssetStorage/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AssetstorageConfig(AppConfig):
+    name = 'AssetStorage'
---
+++ AssetStorageDjango/AssetStorageDjango/settings_local.py
@@ -0,0 +1,5 @@
+import os
+
+DEBUG = True
+SECRET_KEY = '%anv9h!@*rc$6dj7_u8s^ik*(wcoyyh=*mffl1dh(1ti!s1=ru'
+
