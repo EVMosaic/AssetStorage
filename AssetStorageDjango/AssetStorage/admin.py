5d86ab69c6504bb7839c5d9226bf0c37cf4365c3
EVMosaic <evarela@mosaiclearning.com>
2017-02-09 18:35:03 -0500

    Added Initial Models and Migrations

--- AssetStorageDjango/AssetStorage/admin.py
+++ AssetStorageDjango/AssetStorage/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class RelatedAssetsInline(admin.TabularInline):
+    model = RelatedAssets
+    extra = 1
+    fk_name = "main_asset"
+
+class MainAssetsInline(admin.TabularInline):
+    model = RelatedAssets
+    extra = 0
+    fk_name = "supporting_asset"
+
+class TaggedAssetsInline(admin.TabularInline):
+    model = Asset.tags.through
+    extra = 0
+    # fk_name = "assets"
+
+class AssetAdmin(admin.ModelAdmin):
+    inlines = [RelatedAssetsInline, MainAssetsInline]
+
+
+class TagAdmin(admin.ModelAdmin):
+    inlines = [TaggedAssetsInline]
+    fields = ['tag']
+
+
+admin.site.register(Asset, AssetAdmin)
+admin.site.register(Tag, TagAdmin)
--- AssetStorageDjango/AssetStorage/models.py
+++ AssetStorageDjango/AssetStorage/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    tag = models.CharField(max_length=100)
+    # assets = models.ManyToManyField(Asset, through=Asset.tags.through, blank=True)
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        verbose_name = "Tag"
+
+
+class Asset(models.Model):
+    name = models.CharField(max_length=200)
+    path = models.CharField(max_length=300)
+    file_size = models.FloatField()
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name="assets", blank=True)
+    related_assets = models.ManyToManyField('self', through="RelatedAssets", symmetrical=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Asset"
+
+
+class RelatedAssets(models.Model):
+    main_asset = models.ForeignKey(Asset, related_name="main_asset")
+    supporting_asset = models.ForeignKey(Asset, related_name="supporting_asset")
+
+    class Meta:
+        verbose_name = "Asset"
