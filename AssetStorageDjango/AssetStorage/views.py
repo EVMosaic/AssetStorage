6675d97df059cd46d4bff783870d94b579098a26
EVMosaic <evarela@mosaiclearning.com>
2017-02-11 22:49:26 -0500

    Serving media files working

--- AssetStorageDjango/AssetStorage/views.py
+++ AssetStorageDjango/AssetStorage/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import *
 
 # Create your views here.
+class AssetDetailView(DetailView):
+    model = Asset
+    context_object_name = 'asset'
+    template_name = 'AssetStorage/detail_template.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AssetDetailView, self).get_context_data(**kwargs)
+        context['kitten'] = 'Goldberry'
+        context['main_asset'] = self.get_object().previews.all()[3]
+        return context
--- AssetStorageDjango/AssetStorageDjango/urls.py
+++ AssetStorageDjango/AssetStorageDjango/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+from AssetStorage import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^test/(?P<pk>[-\w]+)/$', views.AssetDetailView.as_view(), name='asset-detail')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
